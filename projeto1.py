356cce60aba63f7e2b2c3833bf840b6cd4a53521
Elissonjj <94861696+Elissonjj@users.noreply.github.com>
2024-04-18 13:48:36 -0300

    first commit

---
+++ projeto1.py
@@ -0,0 +1,45 @@
+print()
+print(">>>>>>>>>>>>>>>>>>>>>Aprendendo Python<<<<<<<<<<<<<<<<<<<")
+print()
+
+print("Operadores Lógicos")
+                                            
+print("Adição 2+3=",2+3)
+print("Subtração 5-3=",5-3)
+print("Equação 10-4+2=",10-4+2)
+print("Multiplicação 2*10=",2*10)
+print("Divisão 20/4=",20/4)
+print("Potência 2**3=", 2**3)
+print("Resto da divisão 10%3=",10%3)
+print()
+
+print("Exercício 2.1 10+20*30=",10+20*30)
+print("Exercício 2.1 4**2/30=",4**2/30)
+print("Exercício 2.1 (9**4+2)*6-1=",(9**4+2)*6-1)
+print("Exercício 2.2 10%3*10**2+1-10*4/2=",10%3*10**2+1-10*4/2)
+
+print()
+print("Variáveis")
+a=2
+b=3
+print("a=2, b=3 (a+b)=",(a+b))
+print()
+salario=1500
+aumento=3
+print("Aumento de salário=",(salario+(salario*aumento/100)))
+
+print()
+print("Exercício 2.3")
+print("Olá Meu nome é Elisson")
+print()
+a=3
+b=5
+print("Exercício 2.4 a=3 b=5 (2a*3b)",(2*a*3*b))
+print()
+a=15
+b=30
+c=25
+print("Exercício 2.5 a=15, b=3= e c=25 a*(b+c)",a*(b+c))
+
+
+
